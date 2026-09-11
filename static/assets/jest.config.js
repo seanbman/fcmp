c17c1534fd9225c74fa37341cb86bcdf734b6b88
seanbman <seannicholasburman@gmail.com>
2026-09-10 19:50:19 -0600

    test: load protocol fixture setup

--- static/assets/jest.config.js
+++ static/assets/jest.config.js
@@ -1,7 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-preset: "ts-jest",
+  preset: "ts-jest",
   testEnvironment: "node",
+  setupFilesAfterEnv: ["<rootDir>/tests/setup.ts"],
   transform: {
     "^.+.ts?$": ["ts-jest",{}],
   },
